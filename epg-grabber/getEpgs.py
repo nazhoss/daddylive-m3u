ef653d10019f4489c0ce130194cfa25eb593b12e
James Harding <james@techintrigue.com>
2024-11-15 21:10:45 -0700

    Updated the playlist to channels I want. Updated the tvg-ids.txt to reflect the new playlist and also added my own custom xml file.

--- epg-grabber/getEpgs.py
+++ epg-grabber/getEpgs.py
@@ -62,18 +62,9 @@ def filter_and_build_epg(urls):
 m3u4u_epg = os.getenv("M3U4U_EPG")
 
 urls = [
-	m3u4u_epg,
+	'https://www.bevy.be/generate/8TbvgWSctM.xml.gz',
        'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
-        'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
-	'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
-	'https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz',
-	'https://epgshare01.online/epgshare01/epg_ripper_AU1.xml.gz',
-	'https://epgshare01.online/epgshare01/epg_ripper_IE1.xml.gz',
-	'https://epgshare01.online/epgshare01/epg_ripper_DE1.xml.gz',
-	'https://epgshare01.online/epgshare01/epg_ripper_ZA1.xml.gz',
-	'https://epg.pw/api/epg.xml?channel_id=8486',
-	'https://epg.pw/api/epg.xml?channel_id=12358',
-	'https://epg.pw/api/epg.xml?channel_id=9206',
+        'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz'
 ]
 
 if __name__ == "__main__":
